6f6d360132e57efdd76b07c419234fe5934959f9
Rodrigo Santiago <rodrigosantiag@users.noreply.github.com>
2022-03-02 20:35:26 -0300

    feat: Add test fixtures and testgres (#5)
    * Add test fixtures
    Add testgres initialization
    
    * feat: Increase test coverage (#6)
    
    * Remove unused users columns from database and entities.py
    
    * Increase min test coverage from 1 to 90
    
    * feat: Add Broker entity (#7)
    
    * Add Broker entity
    Remove unused helpers module
    
    * Update .gitignore
    
    Add .DS_Store to .gitignore

--- trades_management/entities.py
+++ trades_management/entities.py
@@ -1,12 +1,28 @@
 from datetime import datetime
 from uuid import UUID, uuid4
 
-from pony.orm.core import PrimaryKey, Required, Optional
+from pony.orm.core import PrimaryKey, Required, Optional, Set, db_session
 from serpens import database
 
 import settings
 
 db = database.Database()
+
+
+class Broker(db.Entity):
+    _table_ = "brokers"
+
+    id = PrimaryKey(int, auto=True)
+    uid = Required(UUID, default=uuid4)
+    name = Required(str)
+    user = Required(lambda: User, column="user_id")
+    created_at = Required(datetime, default=datetime.utcnow)
+    updated_at = Required(datetime, default=datetime.utcnow)
+
+    @staticmethod
+    @db_session
+    def get_by_uid(uid: str) -> "Broker":
+        return Broker.get(uid=uid)
 
 
 class User(db.Entity):
@@ -15,11 +31,12 @@ class User(db.Entity):
     id = PrimaryKey(int, auto=True)
     uid = Required(UUID, default=uuid4)
     encrypted_password = Required(str)
-    confirmed_at = Optional(str, nullable=True)
+    confirmed_at = Optional(datetime, nullable=True)
     unconfirmed_email = Optional(str, nullable=True)
     name = Optional(str, nullable=True)
     email = Optional(str, nullable=True)
     risk = Optional(int, nullable=True)
+    brokers = Set("Broker")
     created_at = Required(datetime, default=datetime.utcnow)
     updated_at = Required(datetime, default=datetime.utcnow)
 
