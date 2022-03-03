fd8e2d2c03f905336d4992c5f701bc991c49d6b7
Rodrigo Santiago <rodrigosantiag@users.noreply.github.com>
2022-03-03 16:51:48 -0300

    Add Trade entity Adjust pony relationships in entities.py Create necessary new fixtures

--- tests/fixtures/fixtures.py
+++ tests/fixtures/fixtures.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 
 from pony.orm.core import db_session
 
-from entities import User, Broker
+from entities import User, Broker, Account, Strategy
 
 
 @db_session
@@ -28,4 +28,32 @@ def create_sample_broker():
         user=create_sample_user(),
         created_at=datetime(2022, 2, 1, 0, 0, 0),
         updated_at=datetime(2022, 2, 1, 0, 0, 0),
+    )
+
+
+@db_session
+def create_sample_account():
+    broker = create_sample_broker()
+
+    return Account(
+        uid=uuid4(),
+        type_account="D",
+        currency="USD",
+        initial_balance=10000.0,
+        current_balance=20000.0,
+        broker=broker,
+        user=broker.user,
+        created_at=datetime(2022, 3, 1, 0, 0, 0),
+        updated_at=datetime(2022, 3, 1, 0, 0, 0),
+    )
+
+
+@db_session
+def create_sample_strategy():
+    return Strategy(
+        uid=uuid4(),
+        name="Fixture Strategy",
+        user=create_sample_user(),
+        created_at=datetime(2022, 3, 1, 0, 0, 0),
+        updated_at=datetime(2022, 3, 1, 0, 0, 0),
     )
