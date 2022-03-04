12fd2f07906c3d32c77410fa60c3fd037c94c909
Rodrigo Santiago <rodrigosantiag@users.noreply.github.com>
2022-03-04 19:27:01 -0300

    Add BrokerSchema Add create_broker handler

---
+++ trades_management/schemas.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+
+from serpens.schema import Schema
+
+
+@dataclass
+class BrokerSchema(Schema):
+    name: str
+    # TODO: temporary adds here user_id. In future associate user to bvroker through authentication
+    user_id: int
+    uid: str = None
+    created_at: datetime = field(default_factory=datetime.utcnow)
+    updated_at: datetime = field(default_factory=datetime.utcnow)
