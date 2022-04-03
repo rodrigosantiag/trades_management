39b9b081b2052f25275ad4ae47ac04ae8467050a
Rodrigo Santiago <rodrigosantiag@users.noreply.github.com>
2022-04-03 19:16:53 -0300

    Make user_id not required in Broker schema (#39)
    Ger user UUID from access token before create a broker
    Adjust swagger.yml removing user_id from create broker request and example

--- trades_management/schemas.py
+++ trades_management/schemas.py
@@ -7,8 +7,7 @@ from serpens.schema import Schema
 @dataclass
 class BrokerSchema(Schema):
     name: str
-    # TODO: temporary adds here user_id. In future associate user to broker through authentication
-    user_id: int
+    user_id: int = None
     uid: str = None
     created_at: datetime = field(default_factory=datetime.utcnow)
     updated_at: datetime = field(default_factory=datetime.utcnow)
