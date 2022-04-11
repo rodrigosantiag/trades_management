a6415be85e3b626a927e8cc98f7f0456495041f1
Rodrigo Santiago <rodrigosantiag@users.noreply.github.com>
2022-04-10 21:27:06 -0300

    Use user_uuid get through authorizer to delete user handler Update swagger

--- trades_management/handlers/delete_broker.py
+++ trades_management/handlers/delete_broker.py
@@ -1,19 +1,23 @@
-from pony.orm import db_session
 from serpens import api
 
-from entities import Broker
+from entities import Broker, User
+from helpers import authorized
 
 
-@api.handler
-@db_session
+@authorized
 def handle(request: api.Request):
-    # TODO: receive delete request as query parameters and authenticate with header
-    payload = request.body
+    user_uuid = request.authorizer.get("user_uuid")
+    broker_uuid = request.path.get("uuid")
+
+    if not broker_uuid:
+        return 404, {"error": "Broker not found"}
+
+    user = User.get(uid=user_uuid)
 
     try:
-        broker = Broker.get(uid=payload["uid"], user=payload["user_id"])
-    except (ValueError, KeyError, IndexError):
-        broker = None
+        broker = Broker.get(uid=broker_uuid, user=user)
+    except ValueError:
+        return 404, {"error": "Broker not found"}
 
     if not broker:
         return 404, {"error": "Broker not found"}
