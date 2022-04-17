3955f25d55df6d56368fdd7a3f001da849acebc8
Rodrigo Santiago <rodrigosantiag@users.noreply.github.com>
2022-04-17 16:27:07 -0300

    feat: Create account handler (#53)
    * Add create account handler
    * Adjust account entity required/optional fields
    * Add AccountSchema
    * Convert account uid to string

--- trades_management/schemas.py
+++ trades_management/schemas.py
@@ -11,3 +11,21 @@ class BrokerSchema(Schema):
     uid: str = None
     created_at: datetime = field(default_factory=datetime.utcnow)
     updated_at: datetime = field(default_factory=datetime.utcnow)
+
+
+@dataclass
+class AccountSchema(Schema):
+    type_account: str
+    currency: str
+    initial_balance: float
+    broker_id: int
+    user_id: int
+    current_balance: float = None
+    created_at: datetime = field(default_factory=datetime.utcnow)
+    updated_at: datetime = field(default_factory=datetime.utcnow)
+
+    def __post_init__(self):
+        super().__post_init__()
+
+        if self.current_balance is None:
+            self.current_balance = self.initial_balance
